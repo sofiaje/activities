027191b31db315a9f57cabc82cdfdc73bd69502c
Sofia <sofia.jespersen@hotmail.com>
2023-03-07 15:54:37 +0100

    better than before

--- main.js
+++ main.js
@@ -11,36 +11,53 @@ async function getData(params) {
 
 getActivityBtn.addEventListener("click", async () => {
 
+    // // min lösning
+    // let radioBtn = document.querySelector('input[name="numberOfParticipants"]:checked');
+    // let select = document.querySelector("#typOfActivity");
+    // let freeActivity = document.querySelector("#freeActivity")
+
+    // let participants = "";
+    // let type = "";
+    // let price = "";
+
+    // radioBtn ? participants = radioBtn.id : "";
+    // select ? type = select.value : "";
+    // freeActivity.checked ? price = 0 : "";
+
+    // let params = new URLSearchParams({
+    //     participants,
+    //     type,
+    //     price
+    // })
+    // data = await getData(params);
+    // console.log(params.toString())
+    // displayData(data);
+
+
+    //uppdaterad lösning
     let radioBtn = document.querySelector('input[name="numberOfParticipants"]:checked');
     let select = document.querySelector("#typOfActivity");
     let freeActivity = document.querySelector("#freeActivity")
 
-    let participants = ""
-    let type = "";
-    let price = "";
+    let params = new URLSearchParams()
 
-    radioBtn ? participants = radioBtn.id : "";
-    select ? type = select.value : "";
-    freeActivity.checked ? price = 0 : ""
-
-    let params = new URLSearchParams({
-        participants,
-        type,
-        price
-    })
+    radioBtn && radioBtn.id !== "any" ? params.append("participants", radioBtn.id) : "";
+    select && select.value !== "any" ? params.append("type", select.value) : "";
+    freeActivity.checked ? params.append("price", 0) : "";
 
     data = await getData(params);
     displayData(data);
-
+    console.log(params.toString())
 })
 
 
 function displayData(data) {
-    if (data.activity) {
-        showActivityDiv.innerHTML = `<h1>${data.activity}</h1>
-        <p>Participants: ${data.participants}<br>
-        Typ of activity: ${data.type}<br>
-        Cost: $${data.price}</p>`
+    let { activity, participants, type, price } = data
+    if (activity) {
+        showActivityDiv.innerHTML = `<h1>${activity}</h1>
+        <p>Participants: ${participants}<br>
+        Typ of activity: ${type}<br>
+        Cost: $${price}</p>`
     } else {
         showActivityDiv.innerHTML = `<h1>${data.error}</h1>`
     }
